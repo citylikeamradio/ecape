dac4f6471c34aeb5f42173da0d6371553b2b9656
Robert Capella <bob.capella@gmail.com>
2023-06-03 16:43:06 -0600

    Complete tests & types, template documentation
     - Template out docstrings, use check_units to enforce static typing
     - Complete tests. Investigate differences between author's calculation
       results and metpy/own results & document
     - Complete support for most unstable, surface based and mixed layer parcel
     - Update lfc/el index selector method to match description in paper from
       closest index to one below level
     - Leverage metpy's check_units decorators to enforce static typing

--- src/ecape_py/ecape.py
+++ src/ecape_py/ecape.py
@@ -2,41 +2,108 @@
 # SPDX-License-Identifier: MIT
 
 """Calculate the entraining CAPE (ECAPE) of a parcel"""
+from typing import List
+
 import metpy.calc as mpcalc
 import numpy as np
 import pint
 from metpy.constants import dry_air_spec_heat_press, earth_gravity
-from metpy.units import units
+from metpy.units import check_units, units
 
 
-def lfc_height(pressure, height, temperature, dew_point_temperature):
-    lfc_p, lfc_t = mpcalc.lfc(pressure, temperature, dew_point_temperature)
-    lfc_idx = np.abs(pressure - lfc_p).argmin()
+@check_units("[pressure]", "[temperature]", "[temperature]")
+def _get_parcel_profile(pressure, temperature, dew_point_temperature, parcel_func=None):
+    """
+
+    :param pressure:
+    :param temperature:
+    :param dew_point_temperature:
+    :param parcel_func:
+    :return:
+    """
+
+    if parcel_func:
+        parcel_p, parcel_t, parcel_td, *parcel_i = parcel_func(pressure, temperature, dew_point_temperature)
+        parcel_profile = mpcalc.parcel_profile(pressure, parcel_t, parcel_td)
+    else:
+        parcel_profile = None
+
+    return parcel_profile
+
+
+@check_units("[pressure]", "[length]", "[temperature]", "[temperature]")
+def calc_lfc_height(pressure, height, temperature, dew_point_temperature, parcel_func):
+    """
+
+    :param pressure:
+    :param height:
+    :param temperature:
+    :param dew_point_temperature:
+    :param parcel_func:
+    :return:
+    """
+
+    parcel_profile = _get_parcel_profile(pressure, temperature, dew_point_temperature, parcel_func)
+
+    lfc_p, lfc_t = mpcalc.lfc(pressure, temperature, dew_point_temperature, parcel_temperature_profile=parcel_profile)
+    lfc_idx = (pressure - lfc_p > 0).nonzero()[0][-1]
     lfc_z = height[lfc_idx]
     return lfc_idx, lfc_z
 
 
-def el_height(pressure, height, temperature, dew_point_temperature):
-    el_p, el_t = mpcalc.el(pressure, temperature, dew_point_temperature)
-    el_idx = np.abs(pressure - el_p).argmin()
+@check_units("[pressure]", "[length]", "[temperature]", "[temperature]")
+def calc_el_height(pressure, height, temperature, dew_point_temperature, parcel_func):
+    """
+
+    :param pressure:
+    :param height:
+    :param temperature:
+    :param dew_point_temperature:
+    :param parcel_func:
+    :return:
+    """
+    parcel_profile = _get_parcel_profile(pressure, temperature, dew_point_temperature, parcel_func)
+
+    el_p, el_t = mpcalc.el(pressure, temperature, dew_point_temperature, parcel_temperature_profile=parcel_profile)
+    el_idx = (pressure - el_p > 0).nonzero()[0][-1]
     el_z = height[el_idx]
     return el_idx, el_z
 
 
-def sr_wind_1km_bunkers_right(pressure, u_wind, v_wind, height):
+@check_units("[pressure]", "[speed]", "[speed]", "[length]")
+def calc_sr_wind(pressure, u_wind, v_wind, height):
+    """
+
+    :param pressure:
+    :param u_wind:
+    :param v_wind:
+    :param height:
+    :return:
+    """
+
     bunkers_right = mpcalc.bunkers_storm_motion(pressure, u_wind, v_wind, height)[0]
+
     u_sr = u_wind - bunkers_right[0]
     v_sr = v_wind - bunkers_right[1]
 
-    avg_u_sr_1km = np.mean(u_sr[np.nonzero(height <= 1000 * units("m"))])
-    avg_v_sr_1km = np.mean(v_sr[np.nonzero(height <= 1000 * units("m"))])
+    u_sr_1km = u_sr[np.nonzero(height <= 1000 * units("m"))]
+    v_sr_1km = v_sr[np.nonzero(height <= 1000 * units("m"))]
 
-    sr_wind = mpcalc.wind_speed(avg_u_sr_1km, avg_v_sr_1km)
-
+    sr_wind = np.mean(mpcalc.wind_speed(u_sr_1km, v_sr_1km))
     return sr_wind
 
 
+@check_units("[pressure]", "[length]", "[temperature]", "[mass]/[mass]")
 def calc_mse(pressure, height, temperature, specific_humidity):
+    """
+
+    :param pressure:
+    :param height:
+    :param temperature:
+    :param specific_humidity:
+    :return:
+    """
+
     moist_static_energy = mpcalc.moist_static_energy(height, temperature, specific_humidity)
     saturation_mixing_ratio = mpcalc.saturation_mixing_ratio(pressure, temperature)
     moist_static_energy_star = mpcalc.moist_static_energy(height, temperature, saturation_mixing_ratio)
@@ -47,14 +114,32 @@ def calc_mse(pressure, height, temperature, specific_humidity):
     return moist_static_energy_bar, moist_static_energy_star
 
 
+@check_units("[energy]/[mass]", "[energy]/[mass]", "[temperature]")
 def calc_integral_arg(moist_static_energy_bar, moist_static_energy_star, temperature):
+    """
+
+    :param moist_static_energy_bar:
+    :param moist_static_energy_star:
+    :param temperature:
+    :return:
+    """
+
     integral_arg = -(earth_gravity / (dry_air_spec_heat_press * temperature)) * (
         moist_static_energy_bar - moist_static_energy_star
     )
     return integral_arg
 
 
+@check_units("[length]/[time]**2", "[length]","[dimensionless]", "[dimensionless]")
 def calc_ncape(integral_arg, height, lfc_idx, el_idx):
+    """
+
+    :param integral_arg:
+    :param height:
+    :param lfc_idx:
+    :param el_idx:
+    :return:
+    """
 
     ncape = np.sum(
         (0.5 * integral_arg[lfc_idx:el_idx] + 0.5 * integral_arg[lfc_idx + 1 : el_idx + 1])
@@ -64,7 +149,17 @@ def calc_ncape(integral_arg, height, lfc_idx, el_idx):
     return ncape
 
 
+@check_units("[speed]", "[dimensionless]", "[length]**2/[time]**2", "[energy]/[mass]")
 def calc_ecape_a(sr_wind, pitchfork, ncape, cape):
+    """
+
+    :param sr_wind:
+    :param pitchfork:
+    :param ncape:
+    :param cape:
+    :return:
+    """
+
     term_a = sr_wind**2 / 2.0
     term_b = (-1 - pitchfork - (2 * pitchfork / sr_wind**2) * ncape) / (4 * pitchfork / sr_wind**2)
     term_c = (
@@ -74,48 +169,57 @@ def calc_ecape_a(sr_wind, pitchfork, ncape, cape):
         )
     ) / (4 * pitchfork / sr_wind**2)
     ecape_a = term_a + term_b + term_c
-    return ecape_a
+    return ecape_a.to("J/kg") if ecape_a >= 0 else 0
 
 
+@check_units("[length]")
 def calc_pitchfork(el_z):
+    """
+
+    :param el_z:
+    :return:
+    """
+
     # additional constants
     sigma = 1.6 * units("dimensionless")
     alpha = 0.8 * units("dimensionless")
     l_mix = 120.0 * units("m")
     pr = (1.0 / 3.0) * units("dimensionless")  # prandtl number
     ksq = 0.18 * units("dimensionless")  # von karman constant
+
     pitchfork = (ksq * alpha**2 * np.pi**2 * l_mix) / (4.0 * pr * sigma**2 * el_z)
     return pitchfork
 
 
+@check_units("[length]", "[pressure]", "[temperature]", "[mass]/[mass]", "[speed]", "[speed]")
 def calc_ecape(
-    height: pint.Quantity,
-    pressure: pint.Quantity,
-    temperature: pint.Quantity,
-    specific_humidity: pint.Quantity,
-    u_wind: pint.Quantity,
-    v_wind: pint.Quantity,
-    cape_type: str = "most unstable",
+    height: List[pint.Quantity],
+    pressure: List[pint.Quantity],
+    temperature: List[pint.Quantity],
+    specific_humidity: List[pint.Quantity],
+    u_wind: List[pint.Quantity],
+    v_wind: List[pint.Quantity],
+    cape_type: str = "most_unstable",
 ) -> pint.Quantity:
     """
     Calculate the entraining CAPE (ECAPE) of a parcel
 
     Parameters:
     ----------
-        height : 'pint.Quantity'
+        height : list of 'pint.Quantity', length units
             Atmospheric heights at the levels given by 'pressure'.
-        pressure : 'pint.Quantity'
+        pressure : list of 'pint.Quantity, pressure units
             Total atmospheric pressure
-        temperature : 'pint.Quantity'
+        temperature : list of 'pint.Quantity', temperature units
             Air temperature
-        specific humidity : 'pint.Quantity'
+        specific humidity : list of 'pint.Quantity', mass/mass (or 'dimensionless') units
             Specific humidity
-        u_wind : 'pint.Quantity'
+        u_wind : list of 'pint.Quantity', speed units
             X component of the wind
-        v_wind : 'pint.Quantity'
+        v_wind : list of 'pint.Quantity', speed units
             Y component of the wind
         cape_type: str
-            Variation of CAPE desired. 'most unstable' (default), 'surface based', or 'mixed layer'
+            Variation of CAPE desired. 'most_unstable' (default), 'surface_based', or 'mixed_layer'
 
     Returns:
     ----------
@@ -124,9 +228,15 @@ def calc_ecape(
     """
 
     cape_func = {
-        "most unstable": mpcalc.most_unstable_cape_cin,
-        "surface based": mpcalc.surface_based_cape_cin,
-        "mixed layer": mpcalc.mixed_layer_cape_cin,
+        "most_unstable": mpcalc.most_unstable_cape_cin,
+        "surface_based": mpcalc.surface_based_cape_cin,
+        "mixed_layer": mpcalc.mixed_layer_cape_cin,
+    }
+
+    parcel_func = {
+        "most_unstable": mpcalc.most_unstable_parcel,
+        "surface_based": None,
+        "mixed_layer": mpcalc.mixed_parcel,
     }
 
     # calculate cape
@@ -134,8 +244,8 @@ def calc_ecape(
     cape, _ = cape_func[cape_type](pressure, temperature, dew_point_temperature)
 
     # calculate the level of free convection (lfc) and equilibrium level (el) indexes
-    lfc_idx, _ = lfc_height(pressure, height, temperature, dew_point_temperature)
-    el_idx, el_z = el_height(pressure, height, temperature, dew_point_temperature)
+    lfc_idx, _ = calc_lfc_height(pressure, height, temperature, dew_point_temperature, parcel_func[cape_type])
+    el_idx, el_z = calc_el_height(pressure, height, temperature, dew_point_temperature, parcel_func[cape_type])
 
     # calculate the buoyancy dilution potential (ncape)
     moist_static_energy_bar, moist_static_energy_star = calc_mse(pressure, height, temperature, specific_humidity)
@@ -143,7 +253,7 @@ def calc_ecape(
     ncape = calc_ncape(integral_arg, height, lfc_idx, el_idx)
 
     # calculate the storm relative (sr) wind
-    sr_wind = sr_wind_1km_bunkers_right(pressure, u_wind, v_wind, height)
+    sr_wind = calc_sr_wind(pressure, u_wind, v_wind, height)
 
     # calculate the entraining cape (ecape)
     pitchfork = calc_pitchfork(el_z)
--- tests/test_ecape.py
+++ tests/test_ecape.py
@@ -1,68 +1,111 @@
-from metpy.units import units
-import numpy as np
 from pathlib import Path
-from pytest import approx
-from src.ecape_py.ecape import calc_ecape, calc_pitchfork, calc_ecape_a, calc_integral_arg, calc_ncape
 
+import numpy as np
+from metpy.calc import dewpoint_from_specific_humidity, most_unstable_parcel
+from metpy.units import units
+from pytest import approx
+
+from src.ecape_py.ecape import (
+    calc_ecape,
+    calc_ecape_a,
+    calc_el_height,
+    calc_integral_arg,
+    calc_lfc_height,
+    calc_mse,
+    calc_ncape,
+    calc_pitchfork,
+    calc_sr_wind,
+)
+
+"""w/in 1% of authors calculations, given errors described below, when mucape is equivalent"""
 
 def sample_data():
-    sounding_loc = Path('./sounding.txt')
-    data = np.genfromtxt(sounding_loc, delimiter=',')
-    height = data[:, 0] * units('m')  # height, m
-    pressure = data[:, 1] * units('Pa')  # pressure, Pa
-    temperature = data[:, 2] * units('K')  # temp, K
-    specific_humidity = data[:, 3] * units('kg/kg')  # water vapor mass fraction (specific humidity), kg / kg
-    u_wind = data[:, 4] * units('m/s')  # u wind, m / s
-    v_wind = data[:, 5] * units('m/s')  # v wind, m / s
-    return height, pressure, temperature, specific_humidity, u_wind, v_wind
+    """
+    values via author's published matlab scripts (sigma = 1.6)
+    https://figshare.com/articles/software/ECAPE_scripts/21859818
+    """
+
+    sounding_loc = Path("./sounding.txt")
+    data = np.genfromtxt(sounding_loc, delimiter=",")
+
+    height = data[:, 0] * units("m")  # height, m
+    pressure = data[:, 1] * units("Pa")  # pressure, Pa
+    temperature = data[:, 2] * units("K")  # temp, K
+    specific_humidity = data[:, 3] * units("kg/kg")  # water vapor mass fraction (specific humidity), kg / kg
+    u_wind = data[:, 4] * units("m/s")  # u wind, m / s
+    v_wind = data[:, 5] * units("m/s")  # v wind, m / s
+
+    dew_point_temperature = dewpoint_from_specific_humidity(pressure, temperature, specific_humidity)
+
+    return height, pressure, temperature, specific_humidity, u_wind, v_wind, dew_point_temperature
 
 
 def test_end_to_end_ecape():
-    height, pressure, temperature, specific_humidity, u_wind, v_wind = sample_data()
-    ecape = calc_ecape(height, pressure, temperature, specific_humidity, u_wind, v_wind)
+    """
+    values via author's published matlab scripts (sigma = 1.6)
+    https://figshare.com/articles/software/ECAPE_scripts/21859818
+    """
+
+    height, pressure, temperature, specific_humidity, u_wind, v_wind, dew_point_temperature = sample_data()
+    for cape_type in ['most_unstable', 'surface_based', 'mixed_layer']:
+        ecape = calc_ecape(height, pressure, temperature, specific_humidity, u_wind, v_wind, cape_type)
+        print(f"{cape_type}: {ecape}")
+
     assert ecape
 
 
 def test_calc_pitchfork():
-    # values via author's published matlab scripts (sigma = 1.6)
-    # https://figshare.com/articles/software/ECAPE_scripts/21859818
+    """
+    values via author's published matlab scripts (sigma = 1.6)
+    https://figshare.com/articles/software/ECAPE_scripts/21859818
+    """
 
-    el_z = 11750.0 * units('m')
+    el_z = 11750.0 * units("m")
     pitchfork = calc_pitchfork(el_z)
-    assert round(pitchfork, 4) == 0.0034 * units('dimensionless')
+    assert pitchfork.magnitude == approx(0.0034, rel=0.001)
 
 
 def test_calc_ecape_a():
-    # values via author's published matlab scripts (sigma = 1.6)
-    # https://figshare.com/articles/software/ECAPE_scripts/21859818
+    """
+    values via author's published matlab scripts (sigma = 1.6)
+    https://figshare.com/articles/software/ECAPE_scripts/21859818
+    """
 
     sr_wind = 16.662798431352986
     pitchfork = 0.003401863644631
     ncape = 7.604878130037112e02
     cape = 3.530029673046427e03
     ecape_a = calc_ecape_a(sr_wind, pitchfork, ncape, cape)
-    assert ecape_a == approx(3.343908138651551e03)
+    assert ecape_a == approx(3.343908138651551e03 , rel=0.0001)
 
 
 def test_calc_integral_arg():
-    sounding_loc = Path('./intarg.txt')
-    data = np.genfromtxt(sounding_loc, delimiter=',')
+    """
+    values via author's published matlab scripts (sigma = 1.6)
+    https://figshare.com/articles/software/ECAPE_scripts/21859818
+    """
+
+    intarg_loc = Path("./intarg.txt")
+    data = np.genfromtxt(intarg_loc, delimiter=",")
     mseo_bar = data[:, 0]
     mseo_star = data[:, 1]
     t0 = data[:, 2]
     int_arg = data[:, 3]
 
     integral_arg = calc_integral_arg(mseo_bar, mseo_star, t0)
-    for i, x in enumerate(integral_arg.magnitude):
-        assert x == approx(int_arg[i], abs=0.0001)
+
+    for test, verify in zip(integral_arg.magnitude, int_arg):
+        assert test == approx(verify, rel=0.0001)
 
 
 def test_calc_ncape():
-    # values via author's published matlab scripts (sigma = 1.6)
-    # https://figshare.com/articles/software/ECAPE_scripts/21859818
+    """
+    values via author's published matlab scripts (sigma = 1.6)
+    https://figshare.com/articles/software/ECAPE_scripts/21859818
+    """
 
-    sounding_loc = Path('./ncape.txt')
-    data = np.genfromtxt(sounding_loc, delimiter=',')
+    ncape_loc = Path("./ncape.txt")
+    data = np.genfromtxt(ncape_loc, delimiter=",")
     integral_arg = data[:, 0]
     height = data[:, 1]
     lfc_idx = 16
@@ -70,3 +113,83 @@ def test_calc_ncape():
 
     ncape = calc_ncape(integral_arg, height, lfc_idx, el_idx)
     assert ncape == 7.604878130037112e02
+
+
+def test_calc_mse():
+    """
+
+    """
+    height, pressure, temperature, specific_humidity, u_wind, v_wind, dew_point_temperature = sample_data()
+
+    mseo_loc = Path("./mseo.txt")
+    data = np.genfromtxt(mseo_loc, delimiter=",")
+    mse0_bar = data[:, 1]
+    mse0_star = data[:, 2]
+
+    mseo_bar_sounding, mseo_star_sounding = calc_mse(pressure, height, temperature, specific_humidity)
+
+    for test, verify in zip(mseo_bar_sounding.magnitude, mse0_bar):
+        assert test == approx(verify, rel=.005)
+    for test, verify in zip(mseo_star_sounding.magnitude, mse0_star):
+        assert test == approx(verify, rel=.005)
+
+
+def test_calc_sr_wind():
+    """
+    values via author's published matlab scripts (sigma = 1.6)
+    https://figshare.com/articles/software/ECAPE_scripts/21859818
+
+    Note: there is a difference in Bunkers right mover compenents
+    between the author's compute_VSR.m and MetPy's metpy.calc.bunkers_storm_motion
+
+    For the example sounding:
+    compute_VSR.m: C_x: 15.634223483535706, C_y: 4.74162399790177, V_sr: 16.662798431352986
+    metpy.calc.bunkers_storm_motion: C_x: 14.635206387655197, C_y: 4.668955528029753, V_sr: 15.929554223814558
+    ~4.6 % lower V_sr -> ~1.1 % lower ECAPE result in this case
+    Given modest effect, no further interrogation conducted, although I expect use of WCA vs. mean to be the cause
+
+    Given equivalent bunkers-right components, the remainder of the function produces equivalent results
+    MetPy's bunkers_storm_motion was used for code readability and consistency.
+    """
+
+    height, pressure, temperature, specific_humidity, u_wind, v_wind, dew_point_temperature = sample_data()
+
+    sr_wind = calc_sr_wind(pressure, u_wind, v_wind, height)
+
+    assert sr_wind.magnitude == 15.929554223814558
+
+
+def test_calc_el_height():
+    """
+    Note: there is a difference in el & lfc idx/heights between the authors published scripts and
+    the following calculations. This may be due to the selection of indexes or the el/lfc calculations.
+
+    COMPUTE_ECAPE.m: MU_EL_idx: 117, MU_EL: 11750 -> ECAPE 3087
+    calc_el_height.py: el_idx: 115, el_z: 11500 -> ECAPE: 3089
+
+    Given a modest (~.1 %) impact on the resultant ECAPE (via the ncape & pitchfork calculations)
+    a process leveraging metpy.calc.el was used for code readability and consistency.
+    """
+
+    height, pressure, temperature, specific_humidity, u_wind, v_wind, dew_point_temperature = sample_data()
+    el_idx, el_z = calc_el_height(pressure, height, temperature, dew_point_temperature, most_unstable_parcel)
+    assert el_idx == 115
+    assert el_z.magnitude == 11500
+
+
+def test_calc_lfc_height():
+    """
+    Note: there is a difference in el & lfc idx/heights between the authors published scripts and
+    the following calculations. This may be due to the selection of indexes or the el/lfc calculations.
+
+    COMPUTE_ECAPE.m: MU_LFC_idx: 16, MU_EL: 1650 -> ECAPE 3086
+    calc_el_height.py: lfc_idx: 18, lfc_z: 1800 -> ECAPE: 3089
+
+    Given a modest (~.1 %) impact on the resultant ECAPE (via the ncape calculation)
+    a process leveraging metpy.calc.lfc was used for code readability and consistency.
+    """
+
+    height, pressure, temperature, specific_humidity, u_wind, v_wind, dew_point_temperature = sample_data()
+    lfc_idx, lfc_z = calc_lfc_height(pressure, height, temperature, dew_point_temperature, most_unstable_parcel)
+    assert lfc_idx == 18
+    assert lfc_z.magnitude == 1800
